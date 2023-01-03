df064ed681b6e6c42b8c3c80f0a2e3e99eb16359
MaoSalman <maosalman1992@gmail.com>
2023-01-03 15:14:06 +0100

    Second Commit

--- 03_strings.py
+++ 03_strings.py
@@ -1,6 +1,7 @@
-print("Hello World!")#double quotation
-print('Testing single quote')#single quotation
-print('''Testing triple quotes''')#triple quotation
+print("Hello World!")  # double quotation
+print('Testing single quote')  # single quotation
+print('''Testing triple quotes''')  # triple quotation
 
 print("   How's you  ??")
 print("Salman")
+print("I am learning Python and Git")
