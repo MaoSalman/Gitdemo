c34d058d386431e598244b2d0623b089559ee96d
MaoSalman <maosalman1992@gmail.com>
2023-01-03 16:54:17 +0100

    Updated on 1-3-2023

--- 03_strings.py
+++ 03_strings.py
@@ -1,7 +1,12 @@
+def name():
+    print("AI is more powerful")
+
+
 print("Hello World!")  # double quotation
 print('Testing single quote')  # single quotation
 print('''Testing triple quotes''')  # triple quotation
 
+print(name())
 print("   How's you  ??")
 print("Salman")
 print("I am learning Python and Git")
